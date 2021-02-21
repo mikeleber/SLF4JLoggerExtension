63affcff6b1c30d712e67a79f4f43e1992f6d805
mike_ <mikeleber71@googlemail.com>
2021-02-21 20:22:11 +0100

    Add test class

---
+++ src/test/java/org/leber/log/jmx/LoggerManagerTest.java
@@ -0,0 +1,21 @@
+package org.leber.log.jmx;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoggerManagerTest {
+
+    @Test
+    public void checkImpl(){
+        new org.leber.log.LoggerFactory();
+        Logger LOG =LoggerFactory.getLogger(LoggerManager.class);
+        String loggerName = LOG.getClass().getName();
+        Assertions.assertEquals("",loggerName);
+    }
+
+
+}
