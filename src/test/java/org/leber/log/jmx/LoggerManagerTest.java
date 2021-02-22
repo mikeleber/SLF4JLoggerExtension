df0a037218d0b5e0891371bf4ee904cebfc273af
mike_ <mikeleber71@googlemail.com>
2021-02-22 22:39:13 +0100

    Add additional methods to jmx

--- src/test/java/org/leber/log/jmx/LoggerManagerTest.java
+++ src/test/java/org/leber/log/jmx/LoggerManagerTest.java
@@ -2,8 +2,8 @@ package org.leber.log.jmx;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.leber.log.LoggerFactory;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,9 +11,17 @@ class LoggerManagerTest {
 
     @Test
     public void checkImpl(){
-        Logger LOG =LoggerFactory.getLogger(LoggerManager.class);
+        Logger LOG = LoggerFactory.getLogger(LoggerManager.class);
         String loggerName = LOG.getClass().getName();
-        Assertions.assertEquals("",loggerName);
+    //    Assertions.assertEquals("",loggerName);
+        for (int i=0;i<1000;i++){
+            try {
+                System.out.println("sleep");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 
